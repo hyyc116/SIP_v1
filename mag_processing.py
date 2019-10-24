b5ffa65f4f1eca3f497fecfc009ea40e0e223d03
hyyc116 <hyyc116@gmail.com>
2019-10-24 20:45:55 +0800

    filter papers 3

--- mag_processing.py
+++ mag_processing.py
@@ -284,7 +284,7 @@ if __name__ == '__main__':
 
     pathObj = PATH(field,tag)
 
-    read_paper_ids(pathObj)
+    # read_paper_ids(pathObj)
 
      ## 画出数量随时间变化曲线
     # plot_paper_year_dis(pathObj._field_paper_num_dis_path,pathObj._field_paper_num_dis_over_time_fig)
